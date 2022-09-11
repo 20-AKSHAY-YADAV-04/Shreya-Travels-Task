344547fa8debb127f7de094be9cfe85c32cc0a4f
Akshay Yadav <akshaymahadevyadav@gmail.com>
2022-09-11 13:02:44 +0530

    Task Update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+
   $(window).scroll(function () {
     if ($(window).scrollTop() > 60) {
       $(".MYNavbar").addClass("navbar-scroll");
